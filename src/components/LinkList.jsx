3cb108dccf8d94fc349184f8eb60975ec8af0663
pasabaa <sanchez.ballesteros.pablo@gmail.com>
2023-02-01 14:13:27 -0600

    add new style and fix items on array

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LinkList({ apiArr }) {
 
@@ -88,54 +88,76 @@ function LinkList({ apiArr }) {
         }
     ]
 
+    const [showVideo, setShowVideo] = useState(true);
+    const [showAudio, setShowAudio] = useState(false);
+
 return (
 <section className='my-6'>
     <section>
-        <h1 className='text-3xl font-bold mb-4'>Vídeo</h1>
-        <div className='grid grid-cols-2 gap-2'>
-        {
-        linksVideo.map((link, i)=> {
-            return(
-                <div key={i} className='text-sm p-4 rounded-xl bg-black/40'>
-                    {apiArr?.[link.number]?.length &&
-                    <a target={'_blank'} rel={'noopener noreferrer'}
-                        download={'Video'} href={apiArr[link.number]?.['0']}>
-                        <p className='text-sm px-2 py-1 bg-red-600 rounded-xl max-w-max'>{apiArr[link.number]?.['3']}</p>
-                        <h1 className='text-xl font-bold my-2'>{apiArr[link.number]?.['1']}</h1>
-                        <p className='uppercase font-semibold text-lg'>{apiArr[link.number]?.['4'].split(';')[0]}</p>
-                    </a>
-                    }
-                </div>
-            )
-        })
-    }
-
-
-        </div>
+        <button onClick={()=>setShowVideo(!showVideo)} className='text-3xl font-bold mb-4 w-full flex justify-between items-center pb-2 border-b'>
+            <h1>Video</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className={`bi bi-chevron-down ${showVideo ? 'up' : 'down'}`} viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg>
+        </button>
+        {showVideo && 
+            <div className='grid grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 gap-2'>
+            {
+            linksVideo.map((link, i)=> {
+                
+                return(
+    
+                    !apiArr?.[link.number]?.['3'] || !apiArr?.[link.number]?.['1'] ?
+                    null
+                    :
+                    <div key={i} className='text-sm p-4 rounded-xl bg-gray-100 xl:last:col-span-3'>
+                        {apiArr?.[link.number]?.length &&
+                            <a target={'_blank'} rel={'noopener noreferrer'}
+                                download={'Video'} href={apiArr[link.number]?.['0']}>
+                                <p className='text-sm px-2 py-1 text-gray-100 bg-red-600 rounded-xl max-w-max'>{apiArr[link.number]?.['3']}</p>
+                                <h1 className='text-base font-bold my-2'>{apiArr[link.number]?.['1']}</h1>
+                                <p className='uppercase font-semibold text-sm'>{apiArr[link.number]?.['4'].split(';')[0]}</p>
+                            </a>
+                            
+                        }
+                    </div>
+                    )
+                })
+            }
+            </div>
+        }
+        
     </section>
 
     <section className='mt-6'>
-        <h1 className='text-3xl font-bold mb-4'>Audio</h1>
-        <div className='grid grid-cols-2 gap-2'>
-            
-        {
-        linksAudio.map((link, i)=> {
-            return(
-                <div key={i} className='text-sm p-4 rounded-xl bg-black/40 last:col-span-2'>
-                    {apiArr?.[link.number]?.length &&
-                    <a target={'_blank'} rel={'noopener noreferrer'}
-                        download={'Video'} href={apiArr[link.number]?.['0']}>
-                        <p className='text-sm px-2 py-1 bg-emerald-500 rounded-xl max-w-max'>{apiArr[link.number]?.['3']}</p>
-                        <h1 className='text-xl font-bold my-2'>{apiArr[link.number]?.['1']}</h1>
-                        <p className='uppercase font-semibold text-lg'>{apiArr[link.number]?.['4'].split(';')[0]}</p>
-                    </a>
-                    }
-                </div>
-            )
-        })
-    }
+        <button onClick={()=>setShowAudio(!showAudio)} className='text-3xl font-bold mb-4 w-full flex justify-between items-center pb-2 border-b'>
+            <h1>Audio</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className={`bi bi-chevron-down ${showAudio ? 'up' : 'down'}`} viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg>
+        </button>
+        {showAudio && 
+             <div className='grid grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 gap-2'>
+             {
+             linksAudio.map((link, i)=> {
+                 return(
+                     <div key={i} className='text-sm p-4 rounded-xl bg-gray-100 last:col-span-2 xl:last:col-span-4'>
+                         {apiArr?.[link.number]?.length &&
+                         <a target={'_blank'} rel={'noopener noreferrer'}
+                             download={'Video'} href={apiArr[link.number]?.['0']}>
+                             <p className='text-sm px-2 py-1 text-gray-100 bg-emerald-600 rounded-xl max-w-max'>{apiArr[link.number]?.['3']}</p>
+                             <h1 className='text-base font-bold my-2'>{apiArr[link.number]?.['1']}</h1>
+                             <p className='uppercase font-semibold text-sm'>{apiArr[link.number]?.['4'].split(';')[0]}</p>
+                         </a>
+                         }
+                     </div>
+                         )
+                     })
+                 }
+             </div>
+        }
+       
 
-        </div>
     </section>
 
   
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { stringify } from 'postcss';
 import React, { useEffect, useState } from 'react'
 import LinkList from '../components/LinkList';
 
@@ -8,13 +7,18 @@ export const HomePage = () => {
     const [urlVideo, setUrlVideo] = useState('');
     const [errorUrl, setErrorUrl] = useState(false);
     const [loading, setLoading] = useState(false);
-    const [description, setDescription] = useState(false);
     const API_KEY = import.meta.env.VITE_API_KEY;
     
     const [links, setLinks]= useState([null]);
 
 
     const [info, setInfo] = useState([null]);
+
+    const handleOnChange = (e) => {
+        e.preventDefault();
+        const value = e.target.value;
+        setUrlVideo(value);
+    }
 
     function validateYouTubeUrl() {    
         setErrorUrl(false);
@@ -65,61 +69,75 @@ export const HomePage = () => {
     }, [])
 
 return (
-<>
+<div className='gap-12 grid grid-cols-1 xl:grid-cols-2'>
     <section className='mt-24'>
+        <div className='backdrop-blur-3xl'>
+            <div>
+                <h1 className='text-6xl font-black'>Sound<span className='text-red-600'>Clip</span></h1>
+                <p className='mt-3 leading-relaxed text-lg'>Descarga y convierte videos de YouTube a audio de alta calidad con SoundClip. Convierta sus videos favoritos a archivos de audio fácilmente y disfrútelos en cualquier lugar, además de poder descargar videos de YouTube de manera rápida y sencilla ¡Prueba ahora!</p>
+            </div>
+            <div className='flex flex-col gap-2 mt-6'>
+                <label className='font-semibold' htmlFor="urlVideo">Pega la URL del video</label>
+                <input 
+                    title='URL del video'
+                    placeholder='https://www.youtube.com/watch?v=dQw4w9WgXcQ' 
+                    onChange={handleOnChange} 
+                    className='border px-3 py-4 rounded-lg text-black' 
+                    type="text" 
+                    name="urlVideo" 
+                    id="urlVideo" />
+                {errorUrl && <span className='text-sm text-red-500'>Debe ser una URL válida. Verifique que la URL esté escrita correctamente o el campo no se encuentre vacío.</span>}
+                
+                <button onClick={validateYouTubeUrl} className='px-2 py-3 border font-bold text-gray-100 bg-red-600 rounded-lg'>Descargar</button>
 
-        <div>
-            <h1 className='text-4xl font-black'>Sound Clip</h1>
-            <p className='mt-3 leading-relaxed'>Descarga y convierte videos de YouTube a audio de alta calidad con SoundClip. Convierta sus videos favoritos a archivos de audio fácilmente y disfrútelos en cualquier lugar.</p>
-        </div>
-        <div className='flex flex-col gap-2 mt-6'>
-            <label className='font-semibold' htmlFor="urlVideo">Pega la URL del video</label>
-            <input onChange={(e)=>setUrlVideo(e.target.value)} className='border px-3 py-4 rounded-lg text-black' type="text" name="urlVideo" id="urlVideo" />
-            {errorUrl && <span className='text-sm text-red-500'>Debe ser una URL válida. Verifique que la URL esté escrita correctamente o el campo no se encuentre vacío.</span>}
-            
-            <button onClick={validateYouTubeUrl} className='px-2 py-3 border rounded-lg'>Descargar</button>
-
+            </div>
+            {loading && <span className="loader mt-2"></span>}
         </div>
     </section>
 
-    {loading && <span className="loader mt-2"></span>}
 
     {info.length != null &&
     <section className='mt-8 relative'>
-        <img className='absolute inset-0 w-full h-full object-cover rounded-2xl p-4' src={info.thumb} alt="" />
-        <div className='relative bg-black/50 backdrop-blur-xl rounded-xl'>
-            <div className='relative'>
-                <img className='relative w-full object-cover rounded-xl' src={info.thumb} alt="" />
-                {/* <iframe className='relative rounded-xl w-full aspect-video' src={`https://www.youtube.com/embed/${urlVideo.includes('www') ? urlVideo.split('=').pop() : urlVideo.split('/')[3]}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
-                <div
-                    className='absolute inset-4 ml-auto text-sm font-bold flex gap-2 max-w-max max-h-min bg-black/20 backdrop-blur-xl text-white rounded-full px-2 py-1'>
-                    <p className='flex items-center justify-center gap-1'><svg xmlns="http://www.w3.org/2000/svg"
-                            width="16" height="16" fill="currentColor" className="bi bi-stopwatch" viewBox="0 0 16 16">
-                            <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z" />
-                            <path
-                                d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z" />
-                        </svg> Duración:</p>
-                    <span>{info.lengthsec > 59 ? (Math.floor((info.lengthsec)/60) + ' minutos aprox') : info.lengthsec + ' segundos'}</span>
+        <div className='border p-8 rounded-xl'>
+            <div className='flex flex-col'>
+                <div className='relative'>
+                    {/* <img className='relative w-full object-cover rounded-xl' src={info.thumb} alt="" /> */}
+                    <iframe className='relative rounded-xl w-full aspect-video' src={`https://www.youtube.com/embed/${urlVideo.includes('www') ? urlVideo.split('=').pop() : urlVideo.split('/')[3]}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                    
+                    <div
+                        className='absolute inset-4 ml-auto text-sm font-bold flex gap-2 max-w-max max-h-min bg-black/20 backdrop-blur-xl text-white rounded-full px-2 py-1'>
+                        <p className='flex items-center justify-center gap-1'><svg xmlns="http://www.w3.org/2000/svg"
+                                width="16" height="16" fill="currentColor" className="bi bi-stopwatch" viewBox="0 0 16 16">
+                                <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z" />
+                                <path
+                                    d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z" />
+                            </svg></p>
+                        <span>{info.lengthsec > 59 ? (Math.floor((info.lengthsec)/60) + ' minutos aprox.') : info.lengthsec + ' segundos'}</span>
+                    </div>
+                <div className='mt-6'>
+                    <p className='font-semibold text-lg text-white bg-red-600 rounded-full max-w-max px-3 py-1'>{info.author}</p>
+                    <h1 className='text-xl font-bold mt-2'>{info.title}</h1>
+                </div>
+                </div>
+                <div className='h-40'>
+                    <h2 className='font-semibold my-2'>Descripción</h2>
+                    <div className='my- overflow-x-hidden overflow-y-scroll h-full'>
+                            <div className='text-sm whitespace-pre-wrap mt-3 min-h-max relative block'>{info.description}</div>
+                    </div>
                 </div>
             </div>
-            <div className='bg-zinc-900/50 backdrop-blur-xl mt-4 rounded-xl p-4'>
-                <h1 className='text-2xl font-bold'>{info.title}</h1>
-                <p className='font-semibold text-lg'>{info.author}</p>
-                <div className='my-4'>
-                    <button onClick={()=>setDescription(!description)} className='px-2 py-2 text-sm rounded-lg bg-black'>{description ? 'Ocultar Descripción' : 'Mostar Descripción'}</button>
-                    {
-                        description &&
-                        <p className='text-sm mt-3'>{info.description}</p>
-                    }
-                </div>
-
+            <div className='mt-16'>
                 <LinkList apiArr={links} />
-                
-                
             </div>
         </div>
+
+
+        {/* <img className='absolute inset-0 w-full h-full object-cover rounded-2xl p-4' src={info.thumb} alt="" /> */}
+        
+                {/* <iframe className='relative rounded-xl w-full aspect-video' src={`https://www.youtube.com/embed/${urlVideo.includes('www') ? urlVideo.split('=').pop() : urlVideo.split('/')[3]}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+                
     </section>
     }
-</>
+</div>
 )
 }
